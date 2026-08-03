95dab0ed6eed58f5c21d0387eabf1daa9618f878
gangwarsoni248-crypto <gangwarsoni248@gmail.com>
2026-08-03 22:40:37 +0530

    Change data source from Excel to CSV for orders

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def load_data_from_sql():
     try:
         df = pd.read_sql("SELECT * FROM orders", conn)
     except:
-        df_excel = pd.read_excel("SupplyChainData.xlsx", parse_dates=['Date'])
+        df_excel = pd.read_csv("data.csv", parse_dates=['Date'])
         df_excel.to_sql('orders', conn, if_exists='replace', index=False)
         df = pd.read_sql("SELECT * FROM orders", conn)
     conn.close()
